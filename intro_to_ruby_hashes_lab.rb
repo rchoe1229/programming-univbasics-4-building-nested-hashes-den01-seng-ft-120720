3513aa719577fcbc39e489596d20b70b1eaa129a
Ryan Choe <ryan.choe@outlook.com>
2020-11-25 23:57:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroad => {
-    {:pieces => 4}, :rent_in_dollars => {}, 
-    
-    
+    {:pieces => 4}, :rent_in_dollars => {}, :names => {}
   }
 
 end
