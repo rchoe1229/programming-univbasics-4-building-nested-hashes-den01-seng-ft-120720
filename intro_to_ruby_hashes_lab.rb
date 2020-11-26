f1572188bac1f31089b2dbdc5fc140aa0e0bc601
Ryan Choe <ryan.choe@outlook.com>
2020-11-26 00:54:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
     }, 
     :names => {
-      :reading_railraod => {},
+      :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline_railroad => {}
@@ -43,10 +43,22 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200
     }, 
     :names => {
-      :reading_railraod => {:mortgage_value => 100},
-      :pennsylvania_railroad => {:mortgage_value => 200},
-      :b_and_o_railroad => {:mortgage_value => 400},
-      :shortline_railroad => {:mortgage_value => 800}
+      :reading_railroad => {
+        :mortgage_value => 100
+        
+      },
+      :pennsylvania_railroad => {
+        :mortgage_value => 200
+        
+      },
+      :b_and_o_railroad => {
+        :mortgage_value => 400
+        
+      },
+      :shortline_railroad => {
+        :mortgage_value => 800
+        
+      }
     }
    }
   }
