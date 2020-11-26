e666277e90fdfcd99291eccd0993032f3a902a6c
Ryan Choe <ryan.choe@outlook.com>
2020-11-26 00:23:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def base_hash
-  {:railroad => {}
+  {:railroads => {}
     
   }
 
 end
 
 def monopoly_with_second_tier
-  {:railroad => {
+  {:railroads => {
     :pieces => 4, 
     :rent_in_dollars => {}, 
     :names => {}
